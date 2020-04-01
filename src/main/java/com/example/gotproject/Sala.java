1895a44a93e53c8cd3827f0e47f603875f2f9a25
Pricila <pricila.albero@gmail.com>
2020-04-01 16:29:19 +0200

    clase sala

---
+++ src/main/java/com/example/gotproject/Sala.java
@@ -0,0 +1,21 @@
+package com.example.gotproject;
+
+public class Sala {
+    private int idSala;
+    private Puerta puerta;
+
+    public Sala (){
+        idSala=0;
+    }
+    public Sala(int id){
+        this.idSala = id;
+    }
+
+    public int getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(int idSala) {
+        this.idSala = idSala;
+    }
+}
