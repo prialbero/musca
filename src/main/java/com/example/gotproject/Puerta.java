17bdd21fe6afcdbba31ae7a6a5f2a889fca0a871
Pricila <pricila.albero@gmail.com>
2020-04-01 12:53:22 +0200

    metodo cerrar puerta

--- src/main/java/com/example/gotproject/Puerta.java
+++ src/main/java/com/example/gotproject/Puerta.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public class Puerta {
     //lista de configuración de la puerta
-    private List<Llave> conf = new ArrayList<>();
+    private List<Llave> conf;
     //arbol que guarda la combinación de la puerta
-    private Arbol<Llave> listaLlaves = new Arbol<>();
+    private Arbol<Llave> listaLlaves;
     //arbol que guarda las llaves usadas
-    private Arbol<Llave> llavesProbadas = new Arbol<>();
+    private Arbol<Llave> llavesProbadas;
 
     //tipo enumerado que define el estado de la puerta
     private enum estado {NoConfig, Abierta, Cerrada, Config}
@@ -23,7 +23,10 @@ public class Puerta {
 
     public Puerta() {
         // cambiar los new array y new arbol aquí :D
-        esta = estado.NoConfig;
+        this.conf = new ArrayList<>();
+        this.listaLlaves = new Arbol<>();
+        this.llavesProbadas = new Arbol<>();
+        this.esta = estado.NoConfig;
     }
 
     //configuración de la puerta con la combinación de llaves
@@ -40,17 +43,16 @@ public class Puerta {
     //abrir puerta
     public void probarLlave(Llave k) {
         //System.out.println("estado "+esta);
-        if (esta == estado.Cerrada) {
+        if (this.esta == estado.Cerrada) {
             //la puerta comprueba si la llave se ha probado
             if (llavesProbadas.pertenece(k)) {
                 System.out.println("Ya se ha probado esta llave");
             } //si no se ha probado y la llave forma parte de la combinación, se elimina de la misma
-            else if (listaLlaves.pertenece(k)) {
-                // System.out.println("pertenece a la combinacion "+listaLlaves.pertenece(k));
+            else
+                if (listaLlaves.pertenece(k)) {
+                //System.out.println("pertenece a la combinacion "+listaLlaves.pertenece(k));
                 llavesProbadas.insertar(k);
                 listaLlaves.borrar(k);
-                System.out.println("lista llaves " + listaLlaves.profundidad());
-                System.out.println("llaves probadas " + llavesProbadas.profundidad());
             }
             /**se abre la puerta si la profundidad de la combinacion es menor que el valor
              * y si el numero de llaves internas es mayor o igual al numero de llaves finales
@@ -60,31 +62,36 @@ public class Puerta {
     }
 
     public boolean estaAbierta() {
-        if (esta == estado.Abierta)
-            return true;
-        else return false;
+            return this.esta == estado.Abierta;
+
     }
 
     //vaciar arboles
     public void vaciar() {
-        //
-        for (int i = 0; i <= listaLlaves.profundidad(); i++) {
-            //borrar dato que está en la posición i del array de llaves
-            listaLlaves.borrar(conf.get(i));
-            System.out.println("vaciar " + listaLlaves.profundidad());
-        }
-        for (int i = 0; i <= llavesProbadas.profundidad(); i++) {
+            //borrar dato del árbol
+            listaLlaves.borrar(listaLlaves.getRaiz());
+
+
+        //System.out.println("vaciado "+listaLlaves.profundidad());
+
+        /*for (int i = 0; i <= llavesProbadas.profundidad(); i++) {
             //borrar dato que está en la posición i del array de llaves
             llavesProbadas.borrar(conf.get(i));
-            System.out.println("vaciar probadas " + llavesProbadas.profundidad());
-        }
+        }*/
     }
 
     //cerrar la puerta
     public void cerrarPuerta() {
-        //cada vez que se cierra la puerta se vacía la memoria  de llaves probadas y la puerta vuelve a su estado inicial
+        //se vacía la memoria  de llaves probadas y la puerta vuelve a su estado inicial
         vaciar();
-        esta = estado.Cerrada;
+
+        //recorrer array de configuración e insertar en árbol de combinación
+
+        for(int i=0;i<conf.size();i++) {
+            listaLlaves.insertar(conf.get(i));
+        }
+        //cierra la puerta
+        this.esta = estado.Cerrada;
     }
 
     public void mostrarCerradura() {
