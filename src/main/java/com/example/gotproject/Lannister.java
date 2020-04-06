904d4bba68430faf4a9cbb93199f162a3c71b65f
Pricila <pricila.albero@gmail.com>
2020-04-06 11:50:28 +0200

    Creación de personajes

---
+++ src/main/java/com/example/gotproject/Lannister.java
@@ -0,0 +1,14 @@
+package com.example.gotproject;
+
+public class Lannister extends Personajes {
+    public Lannister() {
+        super();
+    }
+
+    public Lannister(String nombreS, char marcaS, int turnoS, int salaInicioS) {
+        super(nombreS,marcaS,turnoS,salaInicioS);
+        System.out.println("clase Lannister: "+getNombre()+" marca: "+getMarca()+" turno: "+getTurno()+" sala inicio: "+getSalaInicio());
+    }
+
+
+}
---
+++ src/main/java/com/example/gotproject/Targaryen.java
@@ -0,0 +1,14 @@
+package com.example.gotproject;
+
+public class Targaryen extends Personajes {
+    public Targaryen() {
+        super();
+    }
+
+    public Targaryen(String nombreS, char marcaS, int turnoS, int salaInicioS) {
+        super(nombreS,marcaS,turnoS,salaInicioS);
+        System.out.println("clase Targaryen: "+getNombre()+" marca: "+getMarca()+" turno: "+getTurno()+" sala inicio: "+getSalaInicio());
+    }
+
+
+}
