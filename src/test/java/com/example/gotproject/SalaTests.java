3206f585fac83885f481023362320651dce7ef75
Your Name <you@example.com>
2020-04-17 13:24:40 +0200

    solved test

--- src/test/java/com/example/gotproject/SalaTests.java
+++ src/test/java/com/example/gotproject/SalaTests.java
@@ -11,12 +11,16 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Queue;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 
 @RunWith(MockitoJUnitRunner.class)
 public class SalaTests {
     @Mock
     private Puerta puerta;
+    @Mock
+    private Personajes personajes;
+
     @Mock
     private Queue<Personajes> personajesEnSala;
     @Mock
@@ -29,8 +33,17 @@ public class SalaTests {
     public void testParaProcesarTodaLaSalaCuandoEsteLlenaDePersonajes() {
 
         Mockito.doReturn(7).when(this.personajesEnSala).size();
-        Mockito.doReturn(1).when(this.personajesEnSala).peek().getTurno();
-        Mockito.doReturn(1).when(this.personajesEnSala).peek().ejecutarAcciones(any(Sala.class), 1);
+        Mockito.doReturn(this.personajes).when(this.personajesEnSala).peek();
+        Mockito.doReturn(this.personajes).when(this.personajesEnSala).poll();
+        Mockito.doReturn(1).when(this.personajes).getTurno();
+        Mockito.doNothing().when(this.personajes).ejecutarAcciones(any(Sala.class), anyInt());
+
         sala.procesarTurno(1, 30);
+
+        Mockito.verify(this.personajesEnSala).size();
+        Mockito.verify(this.personajesEnSala,Mockito.times(7)).peek();
+        Mockito.verify(this.personajesEnSala,Mockito.times(7)).poll();
+        Mockito.verify(this.personajes,Mockito.times(7)).getTurno();
+        Mockito.verify(this.personajes,Mockito.times(7)).ejecutarAcciones(any(Sala.class), anyInt());
     }
 }
